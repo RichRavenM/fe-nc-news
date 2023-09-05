ce73990f15f44c7493d825986eff1a5c40c1158b
RichRavenM <richard.s.morris@btinternet.com>
2023-09-05 15:35:14 +0100

    adds some errors stuff

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -31,7 +31,20 @@ const Articles = () => {
   }, [searchParams]);
 
   if (isLoading) return <h1>Loading...</h1>;
-  if (isError) return <h1>Error. Please try again</h1>;
+  if (isError)
+    return (
+      <>
+        <h2>
+          Error. Please try again with a valid topic. See the topics section
+          above for an up-to-date list
+        </h2>
+        <img
+          className="error-bot"
+          src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGpkanIxNTNkOGp2emF6bWE4ejFwcTFldHBxZ3RkZXduZGUzNnJ1dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/lRSeZ2ddNwhZ5AgIvk/giphy.gif"
+          alt="A sad face"
+        ></img>
+      </>
+    );
 
   return (
     <>
---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,14 @@
+const PageNotFound = () => {
+  return (
+    <>
+      <h2>Error 404: Page not found</h2>
+      <img
+        className="error-bot"
+        src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExbGpkanIxNTNkOGp2emF6bWE4ejFwcTFldHBxZ3RkZXduZGUzNnJ1dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/lRSeZ2ddNwhZ5AgIvk/giphy.gif"
+        alt="A sad face"
+      ></img>
+    </>
+  );
+};
+
+export default PageNotFound;
